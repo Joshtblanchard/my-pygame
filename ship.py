a638eb91e1bf59c15a727b8190480981ef70d735
Joshtblanchard <Joshtblanchard@gmail.com>
2022-10-20 11:02:13 -0400

    rect.midbottom error

--- ship.py
+++ ship.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 import pygame
 
 class Ship:
@@ -6,7 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('image/ship.bmp')
@@ -14,6 +15,23 @@ class Ship:
 
         # Start each new ship at the bottom center of the screeen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ships horizontal positon.
+        self.x = float(self.rect.x)
+
+        # Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x += self.settings.ship_speed
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
