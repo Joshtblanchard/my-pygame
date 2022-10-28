4d6a4e94c7bb1e1b5fba802b9e537af9bddaeef1
Joshtblanchard <Joshtblanchard@gmail.com>
2022-10-27 20:30:47 -0400

    Added Fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from zoneinfo import available_timezones
 import pygame
 
 from settings import Settings
@@ -38,10 +39,32 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
 
+        # Determine the number of rows of aliens that fit on the screen.
+        ship_hieght = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_hieght)
+
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Create the full fleet of aliens.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Create am alien and place it in the row."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
